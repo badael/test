dacbc5bd74fbe53fe0323b92e4a2aa10be94a018
majdAlmanwer <majdalmanwer25@gmail.com>
2021-07-06 23:33:50 +0300

    new

--- lib/widgets/custom_textFormField.dart
+++ lib/widgets/custom_textFormField.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CustomTextFormField extends StatefulWidget {
+class CustomTextFormField extends StatelessWidget {
   final String hintText;
   final Widget prefixIcon;
   final TextEditingController controller;
   final Function functionValidate;
   final Function onSubmitField;
 
-  const CustomTextFormField({
-    required this.hintText,
-    required this.controller,
-    required this.prefixIcon,
-    required this.functionValidate,
-    required this.onSubmitField,
-  });
+  const CustomTextFormField(
+    this.hintText,
+    this.controller,
+    this.prefixIcon,
+    this.functionValidate,
+    this.onSubmitField,
+  );
 
-  @override
-  State<StatefulWidget> createState() {
-    return _CustomTextFormFieldState();
-  }
-}
-
-class _CustomTextFormFieldState extends State<CustomTextFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: widget.controller,
+      controller: controller,
       decoration: InputDecoration(
-        hintText: widget.hintText,
-        prefixIcon: widget.prefixIcon,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
       ),
       style: TextStyle(
         color: Colors.black,
