b8e7e9515b7d63e332ebef78ad94a61448ce4acb
majdAlmanwer <majdalmanwer25@gmail.com>
2021-07-13 11:25:44 +0300

    new_widget

--- lib/widget/custom_widgets.dart
+++ lib/widget/custom_widgets.dart
@@ -93,3 +93,169 @@ PreferredSizeWidget customAppBar({
     ],
   );
 }
+
+Widget customAlertDialog({
+  @required String title,
+  @required String content,
+}) {
+  return AlertDialog(
+    title: Text(title),
+    titlePadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+    content: Text(content),
+    contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    contentTextStyle: TextStyle(color: Colors.black12, fontSize: 15),
+    actions: [
+      Row(
+        children: [
+          customRaisedButton(text: 'ok', onPressed: () {}),
+          SizedBox(
+            width: 25,
+          ),
+          customRaisedButton(text: 'cancel', onPressed: () {}),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget customCardTransaction({
+  @required String titleExchange,
+  @required String datetime,
+  @required CircleAvatar iconMoneyType,
+  @required String moneyType,
+  @required String contact,
+  @required String note,
+  @required String totaleMoney,
+  @required String attachments,
+  @required String payedMoney,
+}) {
+  return Card(
+    elevation: 5.0,
+    child: Container(
+      padding: EdgeInsets.all(10),
+      color: Colors.grey[200],
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(),
+              SizedBox(
+                width: 15,
+              ),
+              Text(titleExchange),
+              SizedBox(
+                width: 69,
+              ),
+              Text(datetime),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete),
+              ),
+            ],
+          ),
+          Divider(
+            color: Colors.grey[400],
+            endIndent: 20,
+            indent: 20,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(),
+              SizedBox(
+                width: 15,
+              ),
+              Text(contact),
+              SizedBox(
+                width: 120,
+              ),
+              IconButton(onPressed: () {}, icon: iconMoneyType),
+              Text(moneyType),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(),
+              SizedBox(
+                width: 15,
+              ),
+              Text(note),
+              SizedBox(
+                width: 115,
+              ),
+              Text(totaleMoney),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(),
+              SizedBox(
+                width: 15,
+              ),
+              Text(attachments),
+              SizedBox(
+                width: 115,
+              ),
+              Text(payedMoney),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget customContainerWallet({
+  @required String title,
+  @required CircleAvatar circleAvatar,
+  @required Function searchMethod,
+  @required Function addMethod,
+  @required Function editMethod,
+}) {
+  return Container(
+    height: 60,
+    color: Colors.grey,
+    child: ListTile(
+      leading: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        verticalDirection: VerticalDirection.down,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          circleAvatar,
+          SizedBox(
+            width: 15,
+          ),
+          Text(title),
+        ],
+      ),
+      trailing: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        verticalDirection: VerticalDirection.down,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+            onPressed: searchMethod(),
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: addMethod(),
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: editMethod(),
+            icon: Icon(Icons.edit),
+          ),
+        ],
+      ),
+    ),
+  );
+}
